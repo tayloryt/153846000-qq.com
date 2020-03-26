ec3c3fa120b6d02f3cfde92def28ae814d733e04
yangtao2 <taylorytlj@163.com>
2020-03-27 00:23:22 +0800

    promise

--- promise.js
+++ promise.js
@@ -1,78 +1,110 @@
-const ONFULFILLED = 'ONFULFILLED'
-const REJECTED = 'REJECTED'
-const PENDDING = 'PENDDING'
+const ONFULFILLED = "ONFULFILLED";
+const REJECTED = "REJECTED";
+const PENDDING = "PENDDING";
 function isPromise(data) {
-    if ((typeof data === 'object' && data !== null) || typeof data === 'function') {
-        if (typeof data.then === 'function') {
-            return true
-        } else {
-            return false
-        }
-    } else {
-        return false
-    }       
+  return (
+    (typeof data === "object" && data !== null) || typeof data === "function"
+  );
 }
 function resolvePromise(promise, x, resolve, reject) {
-    if (promise === x) {
-        reject('err')
+  if (promise === x) {
+    reject(new TypeError("Chaining cycle detected for promise #<Promise>"));
+  } else if (isPromise(x)) {
+    try {
+      let then = x.then;
+      if (typeof then === "function") {
+        then.call(
+          x,
+          y => {
+            resolvePromise(promise, y, resolve, reject);
+          },
+          r => {
+            resolvePromise(promise, r, resolve, reject);
+          }
+        );
+      } else {
+        resolve(x);
+      }
+    } catch (e) {
+      reject(e);
     }
+  } else {
+    resolve(x);
+  }
 }
 class MyPromise {
-    constructor(fn) {
-        this.status = PENDDING
-        this.value = null
-        this.reason = null
-        this.onfulfilledList = []
-        this.rejectedList = []
-        this.resolve = (value) => {
-            if (this.status === PENDDING) {
-                this.status = ONFULFILLED
-                this.value = value
-                this.onfulfilledList.forEach(v => v())
-            }
-
-        }
-        this.reject = (reason) => {
-            if (this.status === PENDDING) {
-                this.status = REJECTED
-                this.reason = reason
-                this.rejectedList.forEach(v => v())
-            }
-        }
-        try {
-            fn(this.resolve, this.reject)
-        } catch (e) {
-            this.reject(e)
-        }
+  constructor(fn) {
+    this.status = PENDDING;
+    this.value = null;
+    this.reason = null;
+    this.onfulfilledList = [];
+    this.rejectedList = [];
+    this.resolve = value => {
+      if (this.status === PENDDING) {
+        this.status = ONFULFILLED;
+        this.value = value;
+        this.onfulfilledList.forEach(v => v());
+      }
+    };
+    this.reject = reason => {
+      if (this.status === PENDDING) {
+        this.status = REJECTED;
+        this.reason = reason;
+        this.rejectedList.forEach(v => v());
+      }
+    };
+    try {
+      fn(this.resolve, this.reject);
+    } catch (e) {
+      this.reject(e);
     }
-    then(onfulfilled, rejected) {
-        const promise2 = new MyPromise((res, rej) => {
-            if (this.status === ONFULFILLED) {
-                setTimeout(() => {
-                    let x = onfulfilled(this.value)
-                    resolvePromise(promise2,x,res,rej)
-                }) 
-            }
-            if (this.status === REJECTED) {
-                setTimeout(() => {
-                    let x = rejected(this.reason)
-                    resolvePromise(promise2,x,res,rej)
-                })   
-            }
-            if (this.status === PENDDING) {
-                setTimeout(() => {
-                    this.onfulfilledList.push(() => {
-                        let x = onfulfilled(this.value)
-                        resolvePromise(promise2,x,res,rej)
-                    })
-                    this.rejectedList.push(() => {
-                        let x = rejected(this.reason)
-                        resolvePromise(promise2,x,res,rej)
-                    })
-                })
-               
-            }
-        })
-        return promise2
-    }
+  }
+  then(onfulfilled, rejected) {
+    onfulfilled = typeof onfulfilled === "function" ? onfulfilled : val => val;
+    rejected =
+      typeof rejected === "function"
+        ? rejected
+        : val => {
+            throw val;
+          };
+    const promise2 = new MyPromise((res, rej) => {
+      if (this.status === ONFULFILLED) {
+        setTimeout(() => {
+          try {
+            let x = onfulfilled(this.value);
+            resolvePromise(promise2, x, res, rej);
+          } catch (e) {
+            rej(e);
+          }
+        }, 0);
+      }
+      if (this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            let x = rejected(this.reason);
+            resolvePromise(promise2, x, res, rej);
+          } catch (e) {
+            rej(e);
+          }
+        }, 0);
+      }
+      if (this.status === PENDDING) {
+        // setTimeout(() => {
+        this.onfulfilledList.push(() => {
+          let x = onfulfilled(this.value);
+          resolvePromise(promise2, x, res, rej);
+        });
+        this.rejectedList.push(() => {
+          let x = rejected(this.reason);
+          resolvePromise(promise2, x, res, rej);
+        });
+        // })
+        //
+      }
+    });
+    return promise2;
+  }
+  catch(e){
+      this.then(null,reject(e))
+  }
 }
